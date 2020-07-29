05b150e89bab5faf8df11fe2d7cb6e0f1f765b58
Grace <govlmo91@gmail.com>
2020-07-30 01:36:26 +0900

    styledcomponent

--- css-test/src/App.js
+++ css-test/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Button from "./scss/Button3";
-import Box from "./scss/Box3";
+// import Button from "./css-in-js/Button4";
+import Box from "./css-in-js/Box5";
 
 export default function App() {
     return (
         <div>
-            <Button size="big" />
-            <Button size="small" />
+            {/*<Button size="big" />*/}
+            {/*<Button size="small" />*/}
             <Box size="big"/>
             <Box size="small"/>
         </div>
