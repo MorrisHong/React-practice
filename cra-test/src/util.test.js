0876f404ca54659405171c88f7705e689dc57291
Grace <govlmo91@gmail.com>
2020-07-30 00:47:57 +0900

    module css

---
+++ cra-test/src/util.test.js
@@ -0,0 +1,6 @@
+import {addNumber} from "./util";
+
+it('add wto numbers', () => {
+    const result = addNumber(1,2);
+    expect(result).toBe(3);
+})
---
+++ css-test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from "./Button2";
+import Box from "./Box2";
+
+export default function App() {
+    return (
+        <div>
+            <Button size="big" />
+            <Button size="small" />
+            <Box size="big"/>
+            <Box size="small"/>
+        </div>
+    )
+}
