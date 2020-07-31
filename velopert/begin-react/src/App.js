6c642b0a21829bb9f30dfd7a0c181afc5702ccb8
Grace <govlmo91@gmail.com>
2020-07-31 21:32:40 +0900

    map()

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
+import UserList from "./UserList";
 
 function App() {
 	return (
@@ -14,6 +15,8 @@ function App() {
 			<Counter/>
 			<br/>
 			<InputSample/>
+			<br/>
+			<UserList />
 		</Wrapper>
 	)
 }
